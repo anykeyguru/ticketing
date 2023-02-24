2f49ddf72f4ee9430d21efdcb637f447b17b5ea8
ernest <240509865e@gmail.com>
2023-02-24 15:41:32 +0500

    finished test creation

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -2,13 +2,7 @@ import request from 'supertest';
 import { app } from '../../app';
 
 it('returns a 201 on successful signup', async () => {
-    return request(app)
-        .post('/api/users/signup')
-        .send({
-            email: 'test@test.com',
-            password: 'password'
-        })
-        .expect(201);
+    await global.signup(201);
 });
 
 
@@ -66,13 +60,7 @@ it('retunrs 400 with missing email and password fields', async () => {
 });
 
 it('dissalows duplicate email', async () => {
-    await request(app)
-        .post('/api/users/signup')
-        .send({
-            email: 'test@test.com',
-            password: 'password'
-        })
-        .expect(201);
+    const cookie = await global.signup(201);
 
     await request(app)
         .post('/api/users/signup')
--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,6 +1,11 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import request from 'supertest';
 import { app } from '../app';
+
+declare global {
+    var signup: (statusCode: number) => Promise<string[]>;
+}
 
 let mongo: any;
 beforeAll(async () => {
@@ -26,3 +31,19 @@ afterAll(async () => {
     }
     await mongoose.connection.close();
 });
+
+global.signup = async (statusCode: number) => {
+    const email = 'test@test.com';
+    const password = 'password'
+
+    const response = await request(app)
+        .post('/api/users/signup')
+        .send({
+            email, password
+        })
+        .expect(statusCode);
+
+    const cookie = response.get('Set-Cookie');
+
+    return cookie
+}
