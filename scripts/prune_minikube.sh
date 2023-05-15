496ab6e06bb5a627a520ff17475b77209a68ae82
ernest <240509865e@gmail.com>
2023-05-15 16:51:06 +0500

    added script for clean docker images in minikube

--- scripts/prune_minikube.sh
+++ scripts/prune_minikube.sh
@@ -1,1 +1,8 @@
+#!/usr/bin/env bash
 minikube ssh -- docker system prune
+
+eval $(minikube -p minikube docker-env)
+docker rmi $(docker images 'ernestmr/mk-orders' -a -q) \
+              && docker rmi $(docker images 'ernestmr/mk-tickets' -a -q) \
+              && docker rmi $(docker images 'ernestmr/mk-auth' -a -q) \
+              && docker rmi $(docker images 'ernestmr/client' -a -q) \
