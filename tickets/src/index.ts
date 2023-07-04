722eae552da271cc24aaa5382bf928c7e504ad22
ernest@k8s-ubuntu-vb <240509865e@gmail.com>
2023-07-04 11:24:33 +0500

    reforamted message on start

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -11,7 +11,7 @@ import {OrderCancelledListener} from "./events/listeners/order-cancelled-listene
 // Vars
 const PORT: number = 3000;
 const start = async () => {
-    console.log('Starting...');
+    console.log(`Starting on port ${PORT} ...`);
     if (!process.env.JWT_KEY) {
         throw new Error("JWT_KEY must be defined");
     }
