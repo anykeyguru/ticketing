4b43642868548386596e435ad447512f8de60fba
ernest@k8s-ubuntu-vb <240509865e@gmail.com>
2023-07-04 11:00:07 +0500

    add startup message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 // Vars
 const PORT: number = 3000;
 const start = async () => {
-    console.log("Starting ...")
+    console.log('Starting...');
     if (!process.env.JWT_KEY) {
         throw new Error("JWT_KEY must be defined");
     }
