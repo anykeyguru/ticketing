7e58f1cbcd92a1cf8e9c08dfd94a744c0caeda1c
ernest@k8s-ubuntu-vb <240509865e@gmail.com>
2023-07-04 12:44:26 +0500

    test start message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 // Vars
 const PORT: number = 3000;
 const start = async () => {
-    console.log(`Starting on port ${PORT} ...`);
+    console.log(`Starting on port ${PORT}...`);
     if (!process.env.JWT_KEY) {
         throw new Error("JWT_KEY must be defined");
     }
